fdb6267ae34cdae6af5c9991a227fd71e54ce244
The_myth <hyz1011088@gmail.com>
2017-03-05 22:21:36 +0800

    Create TestCircleRectangle.java

---
+++ 11-inheritance and polymorphism/TestCircleRectangle.java
@@ -0,0 +1,12 @@
+
+public class TestCircleRectangle {
+	
+	public static void main(String [] args){
+		Circle4 circle =new Circle4(1);
+		System.out.println("a circle "+circle.toString());
+		System.out.println("the radius is "+ circle.getRadius());
+		System.out.println("the area is "+ circle.getArea());
+		System.out.println("the diameter is "+ circle.getDiameter());
+	}
+
+}
